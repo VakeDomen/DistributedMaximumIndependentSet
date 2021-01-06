9cfb68747fe1e4a1280b891b6e614fe1aa5d570c
VakeDomen <vake.domen94@gmail.com>
2021-01-06 21:49:22 +0100

    piping

--- MIS.py
+++ MIS.py
@@ -1,24 +1,55 @@
-import multiprocessing
 import time
+import numpy as np
+import multiprocessing
+from multiprocessing import Pipe
 
 ADJ_MATRIX_FILE = 'graph1.txt'
 
 class Graph:
     def __init__(self, adj_mat):
         self.adj_mat = adj_mat
-        self.V = set([i for i in range(len(adj_mat[0]))])
-        self.d = calc_degrees(adj_mat)
+        self.create_nodes(self.calc_degrees())
+        self.connect_nodes()
 
-def calc_degrees(adj_mat):
-    print("\tCalculating vertex degrees...")
-    keys = [(vertex, adj_mat) for vertex in range(len(adj_mat[0]))]
-    return multiprocessing.Pool().map(calc_degree_unpack, keys)
+    def create_nodes(self, degrees):
+        node_ids = set([i for i in range(len(self.adj_mat[0]))])
+        self.V = [Node(i, degrees[i]) for i in node_ids]
+
+    def calc_degrees(self):
+        return multiprocessing.Pool().map(self.calc_degree, range(len(self.adj_mat[0])))
     
-def calc_degree_unpack(args):
-    return calc_degree(*args)
+    def calc_degree(self, vertex):
+        return sum(self.adj_mat[vertex])
+    
+    def connect_nodes(self):
+        multiprocessing.Pool().map(self.connect_node, self.V)
 
-def calc_degree(vertex, adj_mat):
-    return sum(adj_mat[vertex])
+    def connect_node(self, node):
+        neighbour_indexes = np.nonzero(self.adj_mat[node.id])[0]
+        for i in range(len(neighbour_indexes)):
+            node.connect(self.V[neighbour_indexes[i]])
+
+
+class Node:
+    def __init__(self, id, degree):
+        self.id = id
+        self.MIS = False
+        self.used = False
+        self.degree = degree
+        self.neighbours = {}
+
+    def set_neighbour(self, neighbour_id, pipe):
+        self.neighbours[neighbour_id] = pipe
+
+    def connect(self, node):
+        conn1, conn2 = Pipe()
+        self.set_neighbour(node.id, conn1)
+        node.set_neighbour(self.id, input)
+        
+
+
+
+
 
 def string_to_matrix(source):
     print("\tParsing matrix...")
@@ -36,10 +67,9 @@ def get_adj_matrix():
 
 def preporcess():
     graph = Graph(get_adj_matrix())
-    print(graph.adj_mat)
     return graph
 
-def luby(graph):
+def slowMIS(graph):
     
     return
 
@@ -50,11 +80,12 @@ def main():
     graph = preporcess()
     mid_time = time.time()
     print("Preprocessing finished in %s seconds!" % (mid_time - start_time))
-    print("Calculating Luby...")
-    luby(graph)
+    print("Calculating MIS...")
+    slowMIS(graph)
     end_time = time.time()
-    print("Calculating Luby finished in %s seconds!" % (end_time - mid_time))
+    print("Calculating MIS finished in %s seconds!" % (end_time - mid_time))
     print("--- Total execution: %s seconds ---" % (end_time - start_time))
+    print(graph.V)
 
 
 if __name__ == "__main__":
